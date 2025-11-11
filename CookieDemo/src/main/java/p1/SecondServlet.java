dbfbefc4591b1264b1791d81a6369279eaec3059
Nikhil Bodke <nikhilbodke066@gmail.com>
2025-11-11 23:42:38 +0530

    cookie basic demo

---
+++ CookieDemo/src/main/java/p1/SecondServlet.java
@@ -0,0 +1,37 @@
+package p1;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+@WebServlet("/SecondServlet")
+public class SecondServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+   
+    public SecondServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		
+		Cookie[] cookies=request.getCookies();
+		
+		for(Cookie cookie : cookies) {
+			out.print("<h2>"+cookie.getName() +" </h2> <br>");
+		out.print("<h2>"+cookie.getValue() + "</h2> <br>");
+		
+		}
+	}
+
+}
